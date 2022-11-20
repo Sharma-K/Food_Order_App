3ace7722b890c3fc05d9b0b17b74174e08753eb4
Keshav Sharma <keshav2743@gmail.com>
2022-11-20 19:06:56 +0530

    added cart overlay

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,34 @@
+import React, {Fragment} from "react";
+import classes from './Modal.module.css';
+import ReactDOM from 'react-dom';
+
+const BackDrop = (props) => {
+
+    return <div className={classes.backdrop}></div>
+}
+
+const ModalOverlays = (props) => {
+
+    return <div className={classes.modal}>
+        <div className={classes.context}>
+            {props.children}
+        </div>
+    </div>
+
+}
+
+const portalElement = document.getElementById('overlays')
+
+const Modal = (props) => {
+
+    return <Fragment>
+       {ReactDOM.createPortal(<BackDrop />, portalElement)}
+       {ReactDOM.createPortal(<ModalOverlays>{props.children}</ModalOverlays>,
+       portalElement
+       )}
+    </Fragment>
+      
+
+};
+
+export default Modal;
