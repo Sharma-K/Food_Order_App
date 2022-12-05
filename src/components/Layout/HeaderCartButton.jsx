d2b05224c6f36de22948bd5dbfcea04b998be3cd
Keshav Sharma <keshav2743@gmail.com>
2022-12-05 15:50:19 +0530

    added animation on button

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,13 +1,19 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import classes from './HeaderCartButton.module.css'
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/CartContext";
 const HeaderCartButton = (props) =>{
 
+
+    
+
+    const [btnIsHighlighted, setBtnHighlighted] = useState(false);
+
     // it will rerender whenever the cartContext changes in the cartProvider
   const cartCtx =  useContext(CartContext);
 
-  console.log('***********',cartCtx);
+
+  
   // it is used to output number of cart items
 
   //reduce method which allows to transform the array of data into a single value
@@ -15,9 +21,32 @@ const HeaderCartButton = (props) =>{
 //   if(cartCtx.items!=null)
 //   {
     // const numberOfCartItems = 0;
-    const numberOfCartItems = cartCtx.item.reduce((current, item)=> current + item.amount,0)
+    const numberOfCartItems = cartCtx.item.reduce((current, item)=> current + item.amount,0);
 //   }
-    return <button className={classes.button}  onClick={props.onClick}>
+
+const btnClasses = `${classes.button} ${btnIsHighlighted?classes.bump: ''}`;
+
+useEffect(()=> {
+
+    if(cartCtx.item.length===0)
+    {
+        return;
+    }
+    setBtnHighlighted(true);
+
+   const timer = setTimeout(() => {
+        setBtnHighlighted(false);
+    }, 300);
+
+    return () => {
+
+
+        clearTimeout(timer);
+    };
+
+}, [cartCtx.item])
+
+    return <button className={btnClasses}  onClick={props.onClick}>
         <span className={classes.icon}>
         <CartIcon />
         </span>
